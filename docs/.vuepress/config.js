f4877271054e5021986c7c93148e35c7fa234862
itzhangbao <itzhangbao@163.com>
2020-10-09 17:19:30 +0800

    jumbo`blog update 🚀

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,22 +2,29 @@ const nav = require("./config/nav.js");
 
 module.exports = {
   title: "JumboのBlog",
-  description:
-    "web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。", // 描述,以 <meta> 标签渲染到页面html中
+  description: "web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。", // 描述,以 <meta> 标签渲染到页面html中
   base: "/blog/", // '/<github仓库名>/'， 默认'/'
   head: [
     // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ["link", { rel: "icon", href: "/img/favicon.ico" }], //favicons，资源放在public文件夹
+    ["link", {
+      rel: "icon",
+      href: "/img/favicon.ico"
+    }], //favicons，资源放在public文件夹
     [
       "meta",
       {
         name: "keywords",
-        content:
-          "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
+        content: "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
       },
     ],
-    ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站点拥有者验证
-    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
+    // ["meta", {
+    //   name: "baidu-site-verification",
+    //   content: "7F55weZDDc"
+    // }], // 百度统计的站点拥有者验证
+    ["meta", {
+      name: "theme-color",
+      content: "#11a8cd"
+    }], // 移动浏览器主题颜色
     // ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
   ],
   markdown: {
@@ -31,7 +38,7 @@ module.exports = {
     // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: "/img/EB-logo.png", // 导航栏logo
+    logo: "/img/ZB-logo.png", // 导航栏logo
     repo: "itzhangbao", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
@@ -46,13 +53,13 @@ module.exports = {
     // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
     // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
-    bodyBgImg: [
-      "https://cdn.jsdelivr.net/gh/itzhangbao/supplies/img/love5201314.jpg",
-      // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
-      // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
-      // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
-    ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
-    bodyBgImgOpacity: 0.8, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
+    // bodyBgImg: [
+    //   "https://cdn.jsdelivr.net/gh/itzhangbao/supplies/img/love5201314.jpg",
+    //   // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175828.jpeg',
+    //   // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175845.jpeg',
+    //   // 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200507175846.jpeg'
+    // ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
+    // bodyBgImgOpacity: 0.8, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
 
     // titleBadge: false, // 文章标题前的图标是否显示，默认true
     // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
@@ -77,15 +84,14 @@ module.exports = {
     },
     blogger: {
       // 博主信息，显示在首页侧边栏
-      avatar: "https://cdn.jsdelivr.net/gh/itzhangbao/supplies/img/logo_00.png",
+      avatar: "https://cdn.jsdelivr.net/gh/itzhangbao/supplies/img/20200919111632.png",
       name: "Jumbo",
       slogan: "正道的光~",
     },
     social: {
       // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-      icons: [
-        {
+      icons: [{
           iconClass: "icon-youjian",
           title: "发邮件",
           link: "mailto:itzhangbao@163.com",
@@ -105,8 +111,7 @@ module.exports = {
     footer: {
       // 页脚信息
       createYear: 2020, // 博客创建年份
-      copyrightInfo:
-        'Jumbo | <a href="https://github.com/itzhangbao/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      copyrightInfo: 'Jumbo | <a href="https://github.com/itzhangbao/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
     },
   },
   plugins: [
@@ -197,33 +202,31 @@ module.exports = {
     ],
     [
       "vuepress-plugin-comment", // 评论
-      // {
-      //   choosen: 'valine',
-      //   options: {
-      //     el: '#valine-vuepress-comment',
-      //     appId: 'qnS1jobNF7CROIQ0XYWBnVOH-gzGzoHsz',
-      //     appKey: 'LIKa0ePqFMkglQfOkN0JNK6c',
-      //     avatar: 'monsterid'
-      //   }
-      // },
       {
-        choosen: "gitalk",
+        choosen: 'valine',
         options: {
-          clientID: "a6e1355287947096b88b",
-          clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
-          repo: "vuepress-theme-vdoing", // GitHub 仓库
-          owner: "xugaoyi", // GitHub仓库所有者
-          admin: ["xugaoyi"], // 对仓库有写权限的人
-          // distractionFreeMode: true,
-          pagerDirection: "last", // 'first'正序 | 'last'倒序
-          id:
-            "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
-          title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
-          labels: ["Gitalk", "Comment"], // GitHub issue 的标签
-          body:
-            "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
-        },
+          el: '#valine-vuepress-comment',
+          appId: 'qnS1jobNF7CROIQ0XYWBnVOH-gzGzoHsz',
+          appKey: 'LIKa0ePqFMkglQfOkN0JNK6c',
+          avatar: 'monsterid'
+        }
       },
+      // {
+      //   choosen: "gitalk",
+      //   options: {
+      //     clientID: "a6e1355287947096b88b",
+      //     clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
+      //     repo: "vuepress-theme-vdoing", // GitHub 仓库
+      //     owner: "xugaoyi", // GitHub仓库所有者
+      //     admin: ["xugaoyi"], // 对仓库有写权限的人
+      //     // distractionFreeMode: true,
+      //     pagerDirection: "last", // 'first'正序 | 'last'倒序
+      //     id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+      //     title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+      //     labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+      //     body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
+      //   },
+      // },
     ],
     [
       "@vuepress/last-updated", // "上次更新"时间格式
